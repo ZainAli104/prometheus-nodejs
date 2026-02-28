64666d795da5193526d1f9d5ec46de1571622c56
Zain Ali <chzainali6363@gmail.com>
2026-02-28 13:43:41 +0500

    feat: add Prometheus metrics for active requests and request duration

--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -1,20 +1,31 @@
 import type {NextFunction, Request, Response} from "express";
 
-import {requestCounter} from "./requestCount.js";
+import {activeRequestsGauge} from "./activeRequests.js";
+import {httpRequestDurationMicroseconds, requestCounter} from "./requestCount.js";
 
 export const metricsMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const startTime = Date.now();
+    activeRequestsGauge.inc();
 
-    res.on('finish', function () {
+    res.on('finish', function() {
         const endTime = Date.now();
-        console.log(`Request took ${endTime - startTime}ms`);
+        const duration = endTime - startTime;
+        console.log(`Request took ${duration}ms`);
 
-        // Increment request counter
         requestCounter.inc({
             method: req.method,
             route: req.route ? req.route.path : req.path,
             status_code: res.statusCode
         });
+
+        activeRequestsGauge.dec();
+
+        httpRequestDurationMicroseconds.observe({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            code: res.statusCode
+        }, duration);
     });
+
     next();
 }
